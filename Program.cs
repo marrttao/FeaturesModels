7486ef2b09ca6cd28d507c9dc7849a3bb5642b5b
romantik <marrttaoburberry@gmail.com>
2026-02-17 15:45:35 +0000

    wefiohwefiopj

--- Program.cs
+++ Program.cs
@@ -3,55 +3,79 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages
-(
-    options =>
-    {
-        options.RootDirectory = "/";
-        options.Conventions.AddFolderRouteModelConvention
-        (
-            "/Features",
-            model =>
+            // Add services to the container.
+            //builder.Services.AddRazorPages();
+            //builder.Services.AddRazorPages(options =>
+            //{
+            //    options.RootDirectory = "/";
+            //    options.Conventions.AddFolderRouteModelConvention("/Features",
+            //        model =>
+            //        {
+            //            foreach (var selector in model.Selectors)
+            //            {
+            //                selector.AttributeRouteModel.Template = selector.AttributeRouteModel.Template
+            //                    .Replace("Features/", "")
+            //                    .Replace("/Pages", "");
+            //            }
+            //        });
+            //});
+
+            //builder.Services.AddRazorPages(options => { 
+            //    options.RootDirectory = "/";
+            //    options.Conventions.AddPageRoute("/Pages/Index", "/");
+            //    options.Conventions.AddPageRoute("/Pages/Privacy", "Privacy");
+            //    options.Conventions.AddPageRoute("/Features/Clients/Pages/Index","Clients");
+            //    options.Conventions.AddPageRoute("/Features/Clients/Pages/Test","Clients/Test");
+            //} );
+
+            builder.Services.AddRazorPages(options => {
+                options.RootDirectory = "/Pages/Features";
+                options.Conventions.AddPageRoute("/Pages/Index", "/");
+                options.Conventions.AddPageRoute("/Pages/Privacy", "Privacy");
+                options.Conventions.AddPageRoute("/Pages/Clients/Index", "Clients");
+                options.Conventions.AddPageRoute("/Pages/Clients/Test", "Clients/Test");
+
+                options.RootDirectory = "/";
+                options.Conventions.AddFolderRouteModelConvention("/Features",
+                    model =>
+                    {
+                        foreach (var selector in model.Selectors)
+                        {
+                            selector.AttributeRouteModel.Template = selector.AttributeRouteModel.Template
+                                .Replace("Features/", "")
+                                .Replace("/Pages", "");
+                        }
+                    });
+            });
+
+            //builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
             {
-                foreach (var selector in model.Selectors)
-                {
-                    selector.AttributeRouteModel!.Template =
-                        selector.AttributeRouteModel.Template!
-                            .Replace("Features/", "")
-                            .Replace("/Pages", "");
-                }
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-        );
-    }
-);
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-var app = builder.Build();
+            app.UseRouting();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+            app.UseAuthorization();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+            app.MapRazorPages()
+                .WithStaticAssets();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.MapGet
-(
-    "/",
-    context =>
-    {
-        context.Response.Redirect("/Clients");
-        return Task.CompletedTask;
-    }
-);
-
-app.Run();
+            //app.MapGet("/",
+            //    context =>
+            //    {
+            //        context.Response.Redirect("/Clients");
+            //        return Task.CompletedTask;
+            //    });
+            app.Run();
--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb45821e5ef76e3a04c07bdde3a04f99caa9eb44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a234c7c614af2bc8624a6cf2f197f4f2b6216c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
